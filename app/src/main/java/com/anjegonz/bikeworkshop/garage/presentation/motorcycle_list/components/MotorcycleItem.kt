12bb139516d96560b8225fa07e8ab7b98fafcc7a
Antonio Gonzalez <anjegonz@gmail.com>
2026-02-23 14:55:29 +0100

    COMPOSE - MotorcycleItem done

--- app/src/main/java/com/anjegonz/bikeworkshop/garage/presentation/motorcycle_list/components/MotorcycleItem.kt
+++ app/src/main/java/com/anjegonz/bikeworkshop/garage/presentation/motorcycle_list/components/MotorcycleItem.kt
@@ -4,26 +4,30 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.anjegonz.bikeworkshop.R
-import com.anjegonz.bikeworkshop.garage.domain.Motorcycle
-import com.anjegonz.bikeworkshop.ui.theme.Purple80
+import com.anjegonz.bikeworkshop.garage.domain.MotorcycleType
+import com.anjegonz.bikeworkshop.garage.domain.MotorcycleUIModel
+import com.anjegonz.bikeworkshop.garage.presentation.core.toImageRes
 
 @Composable
 fun MotorcycleItem(
-    motorcycle: Motorcycle,
+    motorcycle: MotorcycleUIModel,
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -32,7 +36,7 @@ fun MotorcycleItem(
         shape = RoundedCornerShape(32.dp),
         modifier = modifier
             .clickable(onClick = onClick),
-        color = Purple80.copy(alpha = 0.2f)
+        color = MaterialTheme.colorScheme.surfaceVariant
     ) {
         Row(
             modifier = Modifier
@@ -47,18 +51,51 @@ fun MotorcycleItem(
                 contentAlignment = Alignment.Center
             ) {
                 Image(
-                    painter = painterResource(R.drawable.cruiser),
+                    painter = painterResource(motorcycle.type.toImageRes()),
                     contentDescription = motorcycle.type.name,
                     contentScale = ContentScale.Fit,
                     modifier = Modifier
-                        .aspectRatio(
-                            ratio = 0.65f,
-                            matchHeightConstraintsFirst = true
-                        )
+                )
+            }
+
+            Column(
+                modifier = Modifier
+                    .weight(1f),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = motorcycle.model,
+                    style = MaterialTheme.typography.titleLarge,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = motorcycle.manufacturer,
+                    style = MaterialTheme.typography.titleMedium,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
                 )
 
             }
         }
 
     }
+}
+
+@Preview
+@Composable
+private fun PreviewMotorcycleItem() {
+    val motorcycle = MotorcycleUIModel(
+        id = 1,
+        manufacturer = "Ducati",
+        model = "Panigale V4",
+        powerPS = 208,
+        type = MotorcycleType.SPORT,
+        yearOfConstruction = 2021,
+        age = 4
+    )
+    MotorcycleItem(
+        motorcycle = motorcycle,
+        onClick = {}
+    )
 }
